29e0a241e0b62b3ea73e65153812d7e9ec9959cc
Max Bennett <maxbennett602@gmail.com>
2022-12-05 16:27:39 -0700

    playground played in

--- src/components/BindingsExercise.vue
+++ src/components/BindingsExercise.vue
@@ -11,7 +11,7 @@
             Check out what happens below. Now go change the value of message in
             our components script area and see what happens.
           </p>
-          <p>{{}}</p>
+          <p>{{ state.message }}</p>
         </div>
         <div class="border p-1">
           <h5>Two Way Data Binding</h5>
@@ -21,10 +21,11 @@
             html to display the value. Notice Vue will rerender each time the
             value changes.
           </p>
+          <input v-model="state.myName">
+          <p>{{ state.myName }}</p>
           <!-- add a v-model to this input that will reference "myName" within the state object-->
           <input />
           <!-- reference "myName" from the state -->
-          <p>{{}}</p>
         </div>
         <div class="border p-1">
           <h5>Class Binding</h5>
@@ -37,7 +38,7 @@
           </p>
           <div class="red">
             <!-- add a class binding attribute to the p tag that references the state isActive value -->
-            <p>This text will turn green on active</p>
+            <p :class="{ active: state.isActive }">This text will turn green on active</p>
           </div>
         </div>
       </div>
@@ -55,8 +56,9 @@ export default {
     const state = reactive({
       //create a property called myName and set its value to "type your name here"
       message: "Super secret message here!",
+      myName: "Type your name here",
       //change isActive to true
-      isActive: false,
+      isActive: true,
     });
     return { state };
   },
